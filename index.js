5ec6a3833d1f5bf28647dd5b83382a3ab165d654
Federico Ghedina <fedeghe@gmail.com>
2017-01-18 00:39:53 +0100

    some refactoring

--- index.js
+++ index.js
@@ -8,52 +8,51 @@ var fs = require('fs'),
 	packageFileName = 'package.json',
 	backupPackageFileName = '_package.json',
 	outFileName = rewritePackage ? 'package.json' : 'package_solved.json',
-	space = ' ',
-	packageInfo = fs.existsSync(__dirname + '/package.json') ? require(__dirname + '/package.json') : {};
-
-
+	packageInfo = fs.existsSync(__dirname + '/package.json') ? require(__dirname + '/package.json') : {},
+	log = function (msg) {console.log(' ' + msg)}; 
 
 fs.readFile(packageFileName, function (err, data) {
 	if (err) {
-		console.log((space + 'No ' + packageFileName + ' found').red());
+		log(('No ' + packageFileName + ' found').red());
 		process.exit();
 	}
 	orig_pack = JSON.parse(data);
-
-	if(!fs.existsSync(backupPackageFileName)) {
-		fs.writeFile(backupPackageFileName, JSON.stringify(orig_pack, null, 4), 'utf8', function () {
-			console.log(space + backupPackageFileName + ' backup file written');
-		}); 
-	}
 	start();
 });
 
 function start(){
 
-	console.log(space + (packageInfo.name + ' v. ' + packageInfo.version).underline() + ' locking versions for ' + (orig_pack.name+' v. ' + orig_pack.version).white());
+	log((packageInfo.name + ' v. ' + packageInfo.version).underline() + ' locking versions for ' + (orig_pack.name+' v. ' + orig_pack.version).white());
 	
-	console.log(space + 'Output file is: '.red() + outFileName);
+	log('Output file is: '.red() + outFileName);
 
 	var packs = {};
-	
+
 	fs.readdir(modulesFolder, function (err, files) {
 		var pFolders = [];
-		
 		files.forEach(function (folder){
 			if (folder.match(/\..*/)) {
 	  			return;
-	  		} else {
-	  			pFolders.push(folder);
 	  		}
+	  		pFolders.push(folder);
 		})
 		scan(pFolders);
-	  	
 	});
+
 	function scan(pFolders){
 		var counter = pFolders.length;
-
-		console.log(space + 'Dependencies locked: ' + counter);
-		console.log(space + 'starting')
+		log('Dependencies locked: ' + counter);
+		if (counter == 0) {
+			log('...nothing to do');
+			return;
+		} else {
+			if(!fs.existsSync(backupPackageFileName)) {
+				fs.writeFile(backupPackageFileName, JSON.stringify(orig_pack, null, 4), 'utf8', function () {
+					log(backupPackageFileName + ' backup file written');
+				}); 
+			}
+		}
+		log('starting')
 		pFolders.forEach(function (folder){
 
 			fs.stat(modulesFolder + folder, function(err, stats) {
@@ -77,20 +76,20 @@ function start(){
 				fs.readFile(modulesFolder + '/' + pack + '/' + packageFileName, function (err, data) {
 					--counter;
 					if (err) throw err;
-					console.log(space + 'doing ' + pack.white())
+					log('doing ' + pack.white())
 					packs[pack] = JSON.parse(data);
 					if (counter == 0) createFile();
 				});
 			}
 			
 	    	function createFile() {
-	    		console.log(space + 'done!')
+	    		log('done!')
 	    		var j,
 	    			outFile = {};
 	    		for (j in packs) outFile[j] = packs[j].version;
 	    		orig_pack.dependencies = outFile;
 	    		fs.writeFile(outFileName, JSON.stringify(orig_pack, null, 4), 'utf8', function () {
-	    			console.log(space + outFileName + ' written');
+	    			log(outFileName + ' written');
 	    		}); 
 	    	}
 	  	});
